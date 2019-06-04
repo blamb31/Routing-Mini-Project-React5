be774c72a494742cc76063b5cabb04908a7d3a55
Blake Lamb <blake.lamb31@gmail.com>
2019-06-04 12:21:34 -0600

    Finished Mini Project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Nav from "./Components/Nav/Nav";
+import "./App.css";
+import {HashRouter} from 'react-router-dom'
+import routes from './routes'
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div className="App">
+          <Nav />
+          {routes}
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import logo from "./../../images/dm_white_logo.png";
+import "./Nav.css";
+import {Link} from 'react-router-dom'
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="Nav">
+        <div>
+          <img src={logo} alt="dm logo" />
+        </div>
+        <ul>
+          <li>
+              <Link to='/'>Home </Link>
+          </li>
+          <li>
+              <Link to='/topics'>Topics</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Nav;
